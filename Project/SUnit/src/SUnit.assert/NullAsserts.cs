cd256c810eb4a252b8d42a0a6f0e601ba2435a96
idriss92 <idriss2004@hotmail.com>
2014-06-01 18:19:27 +0200

    Assertion Null

---
+++ Project/SUnit/src/SUnit.assert/NullAsserts.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUnit.src.SUnit.assert
+{
+    public partial class Assert
+    {
+        #region IsNull
+        public static void IsNull(object objet)
+        {
+            if (objet != null)
+                throw new InvalidOperationException("Assert.IsNull() Failure");
+
+        }
+        public static void IsNull();
+        #endregion
+
+        #region IsNotNull
+        public static void IsNotNull( object objet)
+        {
+            if (objet == null)
+                throw new InvalidOperationException("Assert.IsNotNull() Failure");
+        }
+        public static void IsNotNull();
+        #endregion
+    }
+}
