9b2fff2dbfd851d74e9448c0605bc10713ea1bbf
idriss92 <idriss2004@hotmail.com>
2014-06-01 13:09:08 +0200

    build
    Compilation et generation

--- Project/SUnit/build.bat
+++ Project/SUnit/build.bat
@@ -1,5 +1,6 @@
 ..\.nuget\NuGet.exe restore ..\SUnit.sln
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library  /out:SUnit.Library.dll /o /recurse:..\src\SUnit.Library\*.cs
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:LVUnit.Console.exe /o /recurse:..\src\LVUnit.Console\*.cs  /r:LVUnit.Library.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Samples.dll /o /recurse:..\samples\SUnit.Samples\*.cs /r:LVUnit.Library.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Test.dll /o /recurse:..\test\LVUnit.Test\*.cs  /r:..\packages\MSTestExtensions.3.0.0\MSTestExtensions.dll,LVUnit.Library.dll,..\packages\Microsoft.VisualStudio.QualityTools.UnitTestFramework.11.0.50727.1\Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library  /out:SUnit.Assert.dll /o /recurse:..\src\SUnit.assert\*.cs
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:SUnit.Console.exe /o /recurse:..\src\SUnit.console\*.cs  /r:SUnit.Library.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Samples.dll /o /recurse:..\Samples\SUnit.Samples\*.cs /r:SUnit.Library.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Core.dll /o /recurse:..\src\SUnit.core\*.cs /r:SUnit.Library.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Test.dll /o /recurse:..\Test\SUnit.Test\*.cs  /r:..\packages\MSTestExtensions.3.0.0\MSTestExtensions.dll,SUnit.Library.dll,..\packages\Microsoft.VisualStudio.QualityTools.UnitTestFramework.11.0.50727.1\Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
