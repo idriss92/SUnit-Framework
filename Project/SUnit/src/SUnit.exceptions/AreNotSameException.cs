20f24ee0dfbbd8ad2a9c903a54afad66d37348cd
idriss92 <idriss2004@hotmail.com>
2014-06-02 16:20:35 +0200

    Personnal Exception  added

---
+++ Project/SUnit/src/SUnit.exceptions/AreNotSameException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUnit.src.SUnit.exceptions
+{
+    public class AreNotSameException: Exception
+    {
+        public AreNotSameException(string userMessage)
+            :base(userMessage ?? "Assert.AreNotSame() Failure")
+        { 
+        }
+    }
+}
---
+++ Project/SUnit/src/SUnit.exceptions/PrincipalException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUnit.src.SUnit.exceptions
+{
+    public class PrincipalException : Exception
+    {
+        readonly string stack;
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PrincipalException ():base(){}
+
+        /// <summary>
+        /// Constructor which defines the property of the message
+        /// </summary>
+        /// <param name="message"></param>
+        public PrincipalException (string userMessage):base(userMessage)
+        {
+            UserMessage= userMessage;
+        }
+        /// <summary>
+        /// Constructor which defines the properties Message and InnerException 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public PrincipalException(string userMessage, Exception inner) : base(userMessage, inner) { }
+
+        // <summary>
+        // Gets the message
+        // </summary>
+        public string UserMessage { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //}
+    }
+}
