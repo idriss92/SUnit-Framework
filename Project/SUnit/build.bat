36b7c14a8a3e9442a18e16c8f7e6f591eb631619
Mamadou NOMOKO <m.nomoko.fr@gmail.com>
2014-06-08 21:08:16 +0200

    new Build.bat file

--- Project/SUnit/build.bat
+++ Project/SUnit/build.bat
@@ -1,6 +1,5 @@
 ..\.nuget\NuGet.exe restore ..\SUnit.sln
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library  /out:SUnit.Assert.dll /o /recurse:..\src\SUnit.assert\*.cs
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:SUnit.Console.exe /o /recurse:..\src\SUnit.console\*.cs  /r:SUnit.Library.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Samples.dll /o /recurse:..\Samples\SUnit.Samples\*.cs /r:SUnit.Library.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Core.dll /o /recurse:..\src\SUnit.core\*.cs /r:SUnit.Library.dll
-%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Test.dll /o /recurse:..\Test\SUnit.Test\*.cs  /r:..\packages\MSTestExtensions.3.0.0\MSTestExtensions.dll,SUnit.Library.dll,..\packages\Microsoft.VisualStudio.QualityTools.UnitTestFramework.11.0.50727.1\Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library  /out:SUnit.Library.dll /o /recurse:..\src\Framework\*.cs
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:SUnit.Runner.exe /o /recurse:..\src\Runner\*.cs  /r:SUnit.Library.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Samples.dll /o /recurse:..\Samples\*.cs /r:SUnit.Library.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Test.dll /o /recurse:..\Test\*.cs  /r:..\packages\MSTestExtensions.3.0.0\MSTestExtensions.dll,SUnit.Library.dll,..\packages\Microsoft.VisualStudio.QualityTools.UnitTestFramework.11.0.50727.1\Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
