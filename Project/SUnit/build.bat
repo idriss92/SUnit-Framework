24dbc7a6d06bcf0c3a7e4f42292f95dfe0a32ec8
idriss92 <idriss2004@hotmail.com>
2014-06-17 14:41:34 +0200

    Add the build Build

--- Project/SUnit/build.bat
+++ Project/SUnit/build.bat
@@ -1,6 +1,6 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc /t:library /out:SUnit.Library.dll /o /recurse:\src\Library\SUnit.assert\*.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:SUnit.Runner.exe /o /recurse:..\Run *.cs  /r:SUnit.Library.dll
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Samples.dll /o /recurse:..\Samples\*.cs /r:SUnit.Library.dll
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Test.dll /o /recurse:..\Test\*.cs  /r:..\packages\MSTestExtensions.3.0.0\MSTestExtensions.dll,SUnit.Library.dll,..\packages\Microsoft.VisualStudio.QualityTools.UnitTestFramework.11.0.50727.1\Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Framework.dll /o /recurse:src\Framework\*.cs
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:SUnit.Runner.exe /o /recurse:src\Run\*.cs  /r:SUnit.Framework.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Samples.dll /o /recurse:Samples\*.cs /r:SUnit.Framework.dll
+%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:library /out:SUnit.Test.dll /o /recurse:Test\*.cs  /r:..\packages\MSTestExtensions.3.0.0\MSTestExtensions.dll,SUnit.Library.dll,..\packages\Microsoft.VisualStudio.QualityTools.UnitTestFramework.11.0.50727.1\Microsoft.VisualStudio.QualityTools.UnitTestFramework.dll
 
 pause
