879d94027c285ee7e7e2ef563b9a9f1156e0261c
idriss92 <idriss2004@hotmail.com>
2014-06-19 14:10:44 +0200

    add all test pass

--- Project/SUnit/src/Framework/Core/TestClassAttribute.cs
+++ Project/SUnit/src/Framework/Core/TestClassAttribute.cs
@@ -5,6 +5,9 @@ using System.Collections;
 
 namespace SUnit.Framework
 {
+    /// <summary>
+    /// Attribute which encapsulate a class test
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple=true,Inherited = true)]
     public class TestClassAttribute : Attribute
     {
